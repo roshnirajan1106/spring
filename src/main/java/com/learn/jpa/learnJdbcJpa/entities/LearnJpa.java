8ffd2bba6e4e01ca3bb2e0954f2cfc5ab8f0a25e
Roshini rajan <roshinirajan@Roshinis-MacBook-Air.local>
2025-03-09 23:46:47 +0530

    Initial commit - add main method

---
+++ src/main/java/com/learn/jpa/learnJdbcJpa/entities/LearnJpa.java
@@ -0,0 +1,10 @@
+package com.learn.jpa.learnJdbcJpa.entities;
+
+public class LearnJpa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
